22e7af5ed0181196023dd3316ad59531befed046
SaimSyed <98846014+SaimSyed@users.noreply.github.com>
2022-03-18 11:30:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker will be deployed over Fargate!!"
+    return "Hello from Flask & Docker"
 
 
 if __name__ == "__main__":
